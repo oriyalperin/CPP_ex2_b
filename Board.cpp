90d2c79fd9f7313adcd80ed4b33371156dfebd46
oriyalperin <oriya2011@gmail.com>
2021-06-15 17:49:19 +0300

    replace try&catch with find

--- Board.cpp
+++ Board.cpp
@@ -1,116 +1,115 @@
 #include <iostream>
 #include <string>
-#include <map> 
+#include <map>
 #include <utility>
 #include "Board.hpp"
 #include "Direction.hpp"
 using namespace std;
 using namespace ariel;
-const char no='_';
+const char no = '_';
+namespace ariel
+{
+    Board::Board()
+    {
+        minRow = minCol = UINT32_MAX;
+        maxRow = maxCol = 0;
+    }
 
-Board::Board()
-{
-    minRow=minCol=UINT32_MAX;
-    maxRow=maxCol=0;
-}
+    void Board::update_limits(unsigned r, unsigned c, unsigned messlen, Direction d)
+    {
+        unsigned h = 0;
+        unsigned v = 0;
+        if (d == Direction::Horizontal)
+        {
+            h = messlen;
+        }
+        else
+        {
+            v = messlen;
+        }
+        if (r + v > maxRow)
+        {
+            maxRow = r + v;
+        }
+        if (r < minRow)
+        {
+            minRow = r;
+        }
+        if (c + h > maxCol)
+        {
+            maxCol = c + h;
+        }
+        if (c < minCol)
+        {
+            minCol = c;
+        }
+    }
+    void Board::post(unsigned int row, unsigned int column, Direction d, string message)
+    {
 
-void Board::update_limits(unsigned r,unsigned c,unsigned messlen,Direction d)
-{
-    unsigned h=0;
-    unsigned v=0;
-    if(d==Direction::Horizontal)
+        unsigned length = message.length();
+        update_limits(row, column, length, d);
+        if (d == Direction::Horizontal)
         {
-            h=messlen;
-        }
-    else
-        {
-            v=messlen;
-        }
-    if(r+v>maxRow)
-    {
-        maxRow=r+v;
-    }
-    if(r<minRow)
-    {
-        minRow=r;
-    }   
-    if(c+h>maxCol)
-    {
-        maxCol=c+h;
-    }
-    if(c<minCol)
-    {
-        minCol=c;
-    }
-}
-void Board::post(unsigned int row, unsigned int column, Direction d, string message )
-{
-    
-    unsigned length= message.length();
-    update_limits(row,column,length,d);
-    if(d==Direction::Horizontal)
-    { 
-        for(unsigned i=column;i<column+length;i++)
-        {
-           board[{row,i}]=message.at(i-column);
-        }
-    }
-    else
-    {
-         for(unsigned i=row;i<row+length;i++)
-        {
-            board[{i,column}]=message.at(i-row);
-        }
-    }
-}
-
-string Board::read(unsigned int row, unsigned int column, Direction d, unsigned int length)
-{
-    string message;
-    if(d==Direction::Horizontal)
-    { 
-        for(unsigned i=column;i<column+length;i++)
-        { 
-            char curr=no;
-            try
+            for (unsigned i = column; i < column + length; i++)
             {
-                curr = board.at({row,i});
+                board[{row, i}] = message.at(i - column);
             }
-            catch(exception out_of_range){}            
-            message+=curr;
+        }
+        else
+        {
+            for (unsigned i = row; i < row + length; i++)
+            {
+                board[{i, column}] = message.at(i - row);
+            }
         }
     }
-    else
-    {
-         for(unsigned i=row;i<row+length;i++)
-        { 
-            char curr=no;
-            try
-            {
-                curr = board.at({i,column});
-            }
-            catch(exception out_of_range){}   
-            message+=curr;
-            
-        }     
-    }
-    return message;
-}
 
-void Board::show()
-{
-    if(board.empty())
+    string Board::read(unsigned int row, unsigned int column, Direction d, unsigned int length)
+    {
+        string message;
+        if (d == Direction::Horizontal)
+        {
+            for (unsigned i = column; i < column + length; i++)
+            {
+                auto it = board.find({row, i});
+                if (it != board.end())
+                {
+                    message += it->second;
+                }
+                else
+                {
+                    message += no;
+                }
+            }
+        }
+        else
+        {
+            for (unsigned i = row; i < row + length; i++)
+            {
+                auto it = board.find({i, column});
+                if (it != board.end())
+                {
+                    message += it->second;
+                }
+                else
+                {
+                    message += no;
+                }
+            }
+        }
+        return message;
+    }
+
+    void Board::show()
+    {
+        if (board.empty())
         {
             throw out_of_range("the board is empty");
         }
-    for (unsigned i=minRow;i<=maxRow;i++)
-    {
-        cout <<read(i,minCol,Direction::Horizontal,maxCol-minCol) << endl;  
+        for (unsigned i = minRow; i <= maxRow; i++)
+        {
+            cout << read(i, minCol, Direction::Horizontal, maxCol - minCol) << endl;
+        }
     }
-}
-
-Board::~Board()
-{
-    board.clear();
-    //delete &board;
 }
--- Board.hpp
+++ Board.hpp
@@ -1,24 +1,25 @@
 #include <iostream>
 #include <string>
-#include <map> 
+#include <map>
 #include <utility>
 #include "Direction.hpp"
-namespace ariel{
-    class Board{
-        private:
-            std::map<std::pair<unsigned,unsigned>, char> board; 
-            unsigned int maxRow;
-            unsigned int maxCol;
-            unsigned int minRow;
-            unsigned int minCol;
-            void update_limits(unsigned r, unsigned c,unsigned messlen,Direction d);
-        public:
-            Board();
-            ~Board();
-            void post(unsigned int row, unsigned int column, Direction d , std::string message );
-            std::string read(unsigned int row, unsigned int column, Direction d, unsigned int length);
-            void show();
+namespace ariel
+{
+    class Board
+    {
+    private:
+        std::map<std::pair<unsigned, unsigned>, char> board;
+        unsigned int maxRow;
+        unsigned int maxCol;
+        unsigned int minRow;
+        unsigned int minCol;
+        void update_limits(unsigned r, unsigned c, unsigned messlen, Direction d);
 
+    public:
+        Board();
+        void post(unsigned int row, unsigned int column, Direction d, std::string message);
+        std::string read(unsigned int row, unsigned int column, Direction d, unsigned int length);
+        void show();
     };
-    
+
 }
